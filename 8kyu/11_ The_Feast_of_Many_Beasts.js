4dfa4c9fa3b7e63d985fbade8bfc4cfaa2d20033
Denis Butyrskiy <d.butyrskiy@gmail.com>
2023-04-08 10:11:33 +0300

    added task solution #11 "The Feast of Many Beasts"

---
+++ 8kyu/11_ The_Feast_of_Many_Beasts.js
@@ -0,0 +1,22 @@
+// Todo. All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+
+// ? Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+
+// ? Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+
+
+function feast(beast, dish) {
+  const firstBeastLetter = beast[0];
+  const lastBeastLetter = beast[beast.length - 1];
+
+  const firstDishLetter = dish[0];
+  const lastDishLetter = dish[dish.length - 1];
+
+  if(firstBeastLetter === firstDishLetter && lastBeastLetter === lastDishLetter) {
+    return true;
+  } else return false;
+}
+
+feast("great blue heron", "garlic naan"); // true
+feast("chickadee", "chocolate cake"); // true
+feast("brown bear", "bear claw"); // false
