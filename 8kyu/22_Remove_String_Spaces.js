56afb90f5194d7a66afafe598e50c7e93265bdc0
Denis Butyrskiy <d.butyrskiy@gmail.com>
2023-04-09 14:05:50 +0300

    added new solution of task "22. "

---
+++ 8kyu/22_Remove_String_Spaces.js
@@ -0,0 +1,22 @@
+// Todo. Write a function that removes the spaces from the string, then return the resultant string.
+
+// ? Examples:
+
+// * Input -> Output
+// * "8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+// * "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+// * "8aaaaa dddd r     " -> "8aaaaaddddr"
+
+
+function noSpace(x){
+  let res = '';
+  for(let i = 0; i < x.length; i++) {
+    if(x[i] === ' ') continue;
+    res += x[i];
+  }
+  return res;
+}
+
+noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'); // '8j8mBliB8gimjB8B8jlB
+noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'); // '88Bifk8hB8BB8BBBB888chl8BhBfd'
+noSpace('8aaaaa dddd r     '); // '8aaaaaddddr'
