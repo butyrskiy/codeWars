4e7cd58dade64cd7a8e7f70093a2009de06d99df
Denis Butyrskiy <d.butyrskiy@gmail.com>
2023-04-17 20:32:52 +0300

    added solution of task "Sentence Smash(8kyu)"

---
+++ 8kyu/49_Sentence_Smash.js
@@ -0,0 +1,11 @@
+// Todo. Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// * ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+function smash(words) {
+  return words.join(' ');
+}
+
+smash(['hello', 'world']); // "hello world"
+smash(['hello', 'amazing', 'world']); // "hello amazing world"
+smash(['this', 'is', 'a', 'really', 'long', 'sentence']); // "this is a really long sentence"
